399aa9162976d03ebc76b5e7150bdc74b6baea57
Lokesh kumar <lokeshthakur088@gmail.com>
2024-03-29 17:30:15 +0530

    third commit

--- frontend/src/components/Authentication/Signup.js
+++ frontend/src/components/Authentication/Signup.js
@@ -59,69 +59,139 @@ const Signup = () => {
             }
     };
 
-    const submitHandler= async() =>{
-        setLoading(true);
-        if(!name || !email || !password || !confirmpassword){
-            toast({
-                title:"Please Enter all Feilds!",
-                status:"warning",
-                duration:5000,
-                isClosable:true,
-                position:"bottom",
-            });
-            setLoading(false);
-            return ;       
-        }
-        if(password !== confirmpassword){
-            toast({
-                title:"Password Do Not Match!",
-                status:"warning",
-                duration:5000,
-                isClosable:true,
-                position:"bottom",
-            });
-            setLoading(false);
-            return ;
-        }
-        try {
-            const config = {
-                headers: {
-                  "Content-type": "application/json",
-                },
-              };
-              const { data } = await axios.post(
-                "http://localhost:5000/api/user",
-                {
-                  name,
-                  email,
-                  password,
-                  pic,
-                },
-                config
-              );
-              console.log(data);
-              toast({
-                title: "Registration Successful",
-                status: "success",
-                duration: 5000,
-                isClosable: true,
-                position: "bottom",
-              });
-              localStorage.setItem("userInfo", JSON.stringify(data));
-              setLoading(false);
-              history.push("/chats");
-        } catch (error) {
-            toast({
-                title:"Error Occured!",
-                description: error.response.data.message,
-                status:"warning",
-                duration:5000,
-                isClosable:true,
-                position:"bottom",
-            });
-            setLoading(false);
-        }
-    };
+//cheacking part
+
+const submitHandler = async () => {
+    setLoading(true);
+    if (!name || !email || !password || !confirmpassword) {
+      toast({
+        title: "Please Fill all the Feilds",
+        status: "warning",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+      setLoading(false);
+      return;
+    }
+    if (password !== confirmpassword) {
+      toast({
+        title: "Passwords Do Not Match",
+        status: "warning",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+      return;
+    }
+    console.log(name, email, password, pic);
+    try {
+      const config = {
+        headers: {
+          "Content-type": "application/json",
+        },
+      };
+      const { data } = await axios.post(
+        "/api/user",
+        {
+          name,
+          email,
+          password,
+          pic,
+        },
+        config
+      );
+      console.log(data);
+      toast({
+        title: "Registration Successful",
+        status: "success",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+      localStorage.setItem("userInfo", JSON.stringify(data));
+      setLoading(false);
+      history.push("/chats");
+    } catch (error) {
+      toast({
+        title: "Error Occured!",
+        description: error.response.data.message,
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+      setLoading(false);
+    }
+  };
+
+
+//checking part
+
+
+    // const submitHandler= async() =>{
+    //     setLoading(true);
+    //     if(!name || !email || !password || !confirmpassword){
+    //         toast({
+    //             title:"Please Enter all Feilds!",
+    //             status:"warning",
+    //             duration:5000,
+    //             isClosable:true,
+    //             position:"bottom",
+    //         });
+    //         setLoading(false);
+    //         return ;       
+    //     }
+    //     if(password !== confirmpassword){
+    //         toast({
+    //             title:"Password Do Not Match!",
+    //             status:"warning",
+    //             duration:5000,
+    //             isClosable:true,
+    //             position:"bottom",
+    //         });
+    //         setLoading(false);
+    //         return ;
+    //     }
+    //     try {
+    //         const config = {
+    //             headers: {
+    //               "Content-type": "application/json",
+    //             },
+    //           };
+    //           const { data } = await axios.post(
+    //             "http://localhost:5000/api/user",
+    //             {
+    //               name,
+    //               email,
+    //               password,
+    //               pic,
+    //             },
+    //             config
+    //           );
+    //           console.log(data);
+    //           toast({
+    //             title: "Registration Successful",
+    //             status: "success",
+    //             duration: 5000,
+    //             isClosable: true,
+    //             position: "bottom",
+    //           });
+    //           localStorage.setItem("userInfo", JSON.stringify(data));
+    //           setLoading(false);
+    //           history.push("/chats");
+    //     } catch (error) {
+    //         toast({
+    //             title:"Error Occured!",
+    //             description: error.response.data.message,
+    //             status:"warning",
+    //             duration:5000,
+    //             isClosable:true,
+    //             position:"bottom",
+    //         });
+    //         setLoading(false);
+    //     }
+    // };
 
   return (
   <VStack spacing='5px' color='black'>
